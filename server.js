2edf1c5726de8d2941c5c0aa271ed69384aceb35
DevJessParker <jess.k.dempsey@gmail.com>
2021-07-28 23:50:21 -0700

    lab13backend

--- server.js
+++ server.js
@@ -20,13 +20,9 @@ app.use(express.json());
 const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect('mongodb://localhost:27017/user-db', mongooseOptions)
 
-let jessica = new User({ name: 'jessica', email: "devjessparker@gma.com", books: [{ name: "Hunger Games", description: "Brutal Teenage Warfare", status: "Have Read" }, { name: "Divergent", description: "Brutal Teenage Warfare Pt.2", status: "Have Read"}, { name: "Divergent", description: "Brutal Teenage Warfare Pt.3", status: "Have Not Read" }]
-})
-jessica.save();
-
-app.get('/auth-test', (req, res) => {
-  res.json({ samepleUser: ( { name: 'ricky'}, { person: 'bobby'} ) })
-});
+// let jessica = new User({ name: 'jessica', email: "devjessparker@gma.com", books: [{ name: "Hunger Games", description: "Brutal Teenage Warfare", status: "Have Read" }, { name: "Divergent", description: "Brutal Teenage Warfare Pt.2", status: "Have Read"}, { name: "Divergent", description: "Brutal Teenage Warfare Pt.3", status: "Have Not Read" }]
+// })
+// jessica.save();
 
 app.post('/books', (req, res) => {
   let newUser = new User(req.body);
